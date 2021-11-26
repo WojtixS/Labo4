659f3875f1d72f91268f1ea6fadfe311ad8e8449
WojtixS <91822459+WojtixS@users.noreply.github.com>
2021-11-26 11:16:14 +0100

    Add files via upload

---
+++ Auta/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Auto a1=new Auto();
+        System.out.println(a1.srPrzebieg());
+
+        Taxi tax1= new Taxi();
+        System.out.println(tax1.srPrzebieg());
+    }
+}
